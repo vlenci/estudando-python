2eb41136e3c413991b198c1588932907e8891d1f
Lenci <vinicius.lenci@semeq.com>
2025-04-16 15:48:04 -0300

    Aula 8

---
+++ recebendo_dados.py
@@ -0,0 +1,12 @@
+print("Qual é seu nome?")
+nome = input()
+
+#forma antiga de print
+print("Seja bem-vindo(a) %s" %nome)
+
+
+print("Qual sua idade?")
+idade = input()
+
+#Forma nova 
+print(f"{nome} tem {idade} anos")

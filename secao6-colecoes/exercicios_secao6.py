5d7cd7f6dd82a5b49b27b27b5c3680dc20c429fb
Vinícius Lenci <vinicius.lenci@semeq.com>
2025-04-22 18:06:34 -0300

    Iniciei os exs da seção 06

---
+++ secao6-colecoes/exercicios_secao6.py
@@ -0,0 +1,35 @@
+# -----------------------------------------
+# Exercício 1
+# -----------------------------------------
+print("1. Crie um programa que lê 6 valores inteiros, armazene em uma lista e em seguida mostre na tela os valores lidos.")
+
+lista = []
+for num in range(0, 6):
+    lista.append(input(f"Insira o valor {len(lista) + 1}/6: "))
+print(lista)
+
+# -----------------------------------------
+# Exercício 2
+# -----------------------------------------
+print("\n2. Faça um programa que possua uma lista denominada A que armazene 6 números inteiros. O programa deve executar os seguintes passos:")
+
+# a)
+print("a) Atribua os seguintes valores a essa lista: 1, 0, 5, -2, -5, 7.")
+
+# b)
+print("b) Armazene em uma variável inteira a soma entre os valores das posições A[0], A[1] e A[5] da lista e mostre na tela esta soma.")
+
+# c)
+print("c) Modifique a lista na posição 5, atribuindo a esta posição o valor 100.")
+
+# d)
+print("d) Mostre na tela cada valor da lista A, um em cada linha.")
+
+
+
+# -----------------------------------------
+# Exercício 3
+# -----------------------------------------
+print("\n3. Faça um programa que leia 10 valores, armazene-os em uma lista e apresente quantos valores pares ele possui.")
+
+# Código da lógica aqui...

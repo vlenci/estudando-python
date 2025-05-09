6209c77cc6373923741ae9201fb1734a7711a684
Lenci <vinicius.lenci@semeq.com>
2025-05-09 16:00:05 -0300

    Projeto 2 praticamente finalizado, falta arrumar erros da 'main.py'

--- secao23-projetos/projeto2-mercado/main.py
+++ secao23-projetos/projeto2-mercado/main.py
@@ -1,25 +1,115 @@
-from mercado import Produto, Estoque
+from mercado import Produto, Estoque, Carrinho
 
-from datetime import date
+from datetime import date, datetime
 
-# Criação de alguns produtos
-p1 = Produto("Leite", "Laticínio", date(2025, 6, 1), 30)
-p2 = Produto("Arroz", "Grão", date(2026, 1, 15), 100)
-p3 = Produto("Iogurte", "Laticínio", date(2024, 12, 31), 20)
 
-# Criando o estoque e adicionando os produtos
-estoque = Estoque()
-estoque.cadastrar_produto(p1)
-estoque.cadastrar_produto(p2)
-estoque.cadastrar_produto(p3)
+def cliente(estoque: Estoque) -> None:
+    car = Carrinho()
 
-# Listando o estoque antes de remover vencidos
-print("Antes de remover vencidos:\n")
-estoque.listar_estoque()
+    while True:
+        print("Carrinho")
+        print("========")
+        print("1 - Adicionar produto")
+        print("2 - Remover produto")
+        print("3 - Visualizar carrinho")
+        print("4 - Fechar pedido")
+        print("5 - Cancelar compra")
 
-# Removendo os produtos vencidos
-estoque.remover_vencido()
+        opcao = int(input())
 
-# Listando o estoque depois da remoção
-print("\nDepois de remover vencidos:\n")
-estoque.listar_estoque()
+        if opcao == 1:
+            prod = input("Insira o nome do produto que deseja adicionar no carrinho: ")
+            for produto in estoque.produtos:
+                if produto.nome == prod:
+                    car.adicionar_produto(produto)
+                    print(f"'{prod}' adicionado com sucesso!")
+                    print("\n")
+                    break
+            print(f"Não existe produto com nome '{prod}' no estoque.")
+            print("")
+        elif opcao == 2:
+            prod = input("Insira o nome do produto que deseja remover do carrinho: ")
+            for produto in car.produtos_carrinho:
+                if produto.nome == prod:
+                    car.remover_produto(produto)
+                    print(f"Uma unidade de '{prod}' removida com sucesso!")
+                    print("\n")
+                    break
+            else:
+                print(f"O produto '{prod}' não está no carrinho.\n")
+
+        elif opcao == 3:
+            car.visualizar_carrinho()
+            print("\n")
+        elif opcao == 4:
+            car.fechar_pedido()
+            print("O mercado MiCi agradece sua preferência! Até logo.")
+            break
+
+        elif opcao == 5:
+            break
+        else:
+            print("Insira uma opção válida.")
+
+
+def administrador() -> None:
+    est = Estoque()
+
+    while True:
+        print("Menu do Administrador")
+        print("========================")
+
+        print("1 - Cadastrar produto")
+        print("2 - Remover produto")
+        print("3 - Visualizar estoque")
+        print("4 - Remover produtos vencidos")
+        print("5 - Sair do sistema")
+        print("")
+        print("6 - Ir para menu do cliente")
+
+        opcao = int(input())
+
+        if opcao == 1:
+            print("Cadastro de produto")
+            print("===================")
+
+            nome = input("Insira o nome do produto: ")
+            tipo = input("Insira o tipo do produto: ")
+            validade = input("Insira a validade do produto (dd/mm/yyyy): ")
+            qtd_estoque = int(input("Insira a quantidade de produto no estoque: "))
+            preco_unidade = float(input("Insira o preço unitário do produto: "))
+            validade_convertida = datetime.strptime(validade, "%d/%m/%Y").date()
+
+            prod = Produto(nome, tipo, validade_convertida, qtd_estoque, preco_unidade)
+            est.cadastrar_produto(prod)
+
+            print("\n")
+        elif opcao == 2:
+            print("Remoção de produto")
+            print("==================")
+
+            id_prod = int(input("Insira o ID do produto a ser removido: "))
+            est.remover_produto(id_prod)
+            print("\n")
+        elif opcao == 3:
+            est.visualizar_estoque()
+            print("\n")
+        elif opcao == 4:
+            est.remover_vencido()
+            print("Produtos vencidos removidos com sucesso!")
+            print("\n")
+        elif opcao == 5:
+            break
+        elif opcao == 6:
+            cliente(est)
+        else:
+            print("Insira uma opção válida.")
+            print("\n")
+
+
+def main() -> None:
+    administrador()
+
+
+if __name__ == "__main__":
+    main()
--- secao23-projetos/projeto2-mercado/mercado.py
+++ secao23-projetos/projeto2-mercado/mercado.py
@@ -1,13 +1,36 @@
 from datetime import date
-from typing import List
+from typing import List, Dict
+
+
+def formatar_preco(preco) -> str:
+    return f"R${preco:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
 
 class Produto:
-    def __init__(self, nome: str, tipo: str, validade: date, qtd_estoque: int):
+    __id_contador = 0
+
+    def __init__(
+        self, nome: str, tipo: str, validade: date, qtd_estoque: int, preco: float
+    ):
+        self.__id = Produto.__id_contador
+        Produto.__id_contador += 1
+
         self.__nome: str = nome
         self.__tipo: str = tipo
         self.__validade: date = validade
         self.__qtd_estoque: int = qtd_estoque
+        self.__preco: float = preco
+        self.__is_vencido: bool = False if validade > date.today() else True
+
+    def __eq__(self, value):
+        return isinstance(value, Produto) and self.id == value.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    @property
+    def id(self) -> int:
+        return self.__id
 
     @property
     def nome(self) -> str:
@@ -22,8 +45,24 @@ class Produto:
         return self.__validade
 
     @property
-    def qtd_estoque(self) -> date:
+    def qtd_estoque(self) -> int:
         return self.__qtd_estoque
+
+    @property
+    def preco(self) -> int:
+        return self.__preco
+
+    @property
+    def is_vencido(self) -> bool:
+        return self.__is_vencido
+
+    @is_vencido.setter
+    def is_vencido(self, novo_is_vencido: bool) -> None:
+        self.__is_vencido = novo_is_vencido
+
+    @qtd_estoque.setter
+    def qtd_estoque(self, nova_qtd_estoque: int) -> None:
+        self.__qtd_estoque = nova_qtd_estoque
 
 
 class Estoque:
@@ -39,39 +78,95 @@ class Estoque:
 
     def remover_vencido(self) -> None:
         for produto in self.produtos:
-            if produto.validade < date.today():
+            if produto.is_vencido:
                 self.produtos.remove(produto)
 
-    def remover_estoque(self) -> None:
-        self.produtos.qt
+    def remover_produto(self, id: int) -> None:
+        for produto in self.produtos:
+            if produto.id == id:
+                self.produtos.remove(produto)
+                print(f"Produto com ID '{id}' removido do estoque com sucesso!")
+                return
+        print(f"Falha na remoção: Não existe produto com o ID '{id}' no estoque.")
 
     # Os print's estão com a formatação melhorada para facilitar visualização
-    def listar_estoque(self):
-        print("Estoque do Mercado MiCi")
-        print(f"{'Nome':<20} {'Tipo':<15} {'Validade':<12} {'Quantidade':<10}")
-        print("-" * 60)
+    def visualizar_estoque(self):
+        print("--- Estoque do Mercado MiCi ---")
+        print(
+            f"{'ID':<5} {'Nome':<20} {'Tipo':<15} {'Validade':<12} {'Quantidade':<13} {'Preço (unidade)':<10}"
+        )
+        print("-" * 90)
 
         for produto in self.produtos:
             validade_formatada = produto.validade.strftime("%d/%m/%Y")
+            preco_formatado: str = formatar_preco(produto.preco)
             print(
-                f"{produto.nome:<20} {produto.tipo:<15} {validade_formatada:<12} {produto.qtd_estoque:<10}"
+                f"{produto.id:<5} {produto.nome:<20} {produto.tipo:<15} {validade_formatada:<12} {produto.qtd_estoque:<13} {preco_formatado:<10}"
             )
 
 
-# LENCI DO PASSADO: FALTA COLOCAR QUANTIDADE DO PRODUTO NO CARRINHO
 class Carrinho:
     def __init__(self):
-        self.__produtos_carrinho: List[Produto] = []
+        # Chave: Produto, Valor: Quantidade de produtos no carrinho
+        self.__produtos_carrinho: Dict[Produto, int] = {}
+        self.__total_pedido: float = 0.0
 
     @property
-    def produtos_carrinho(self) -> List[Produto]:
+    def produtos_carrinho(self) -> Dict[Produto, int]:
         return self.__produtos_carrinho
 
     def adicionar_produto(self, produto: Produto) -> None:
-        self.produtos_carrinho.append(produto)
+        if produto in self.produtos_carrinho:
+            self.produtos_carrinho[produto] += 1
+        elif produto not in self.produtos_carrinho:
+            self.produtos_carrinho[produto] = 1
+
         produto.qtd_estoque -= 1
 
+    def remover_produto(self, produto: Produto) -> None:
+        if self.produtos_carrinho[produto] > 1:
+            self.produtos_carrinho[produto] -= 1
+            produto.qtd_estoque += 1
+        elif self.produtos_carrinho[produto] <= 1:
+            del self.produtos_carrinho[produto]
+            produto.qtd_estoque += 1
+        else:
+            print("O produto não está no carrinho")
+
     def visualizar_carrinho(self) -> None:
-        print("Carrinho")
-        for produto in self.produtos_carrinho:
-            print()
+        print("--- Carrinho ---")
+        print(
+            f"{'ID':<5} {'Nome':<20} {'Tipo':<15} {'Validade':<12} {'Quantidade':<13} {'Preço':<10}"
+        )
+        print("-" * 85)
+
+        for produto, quantidade in self.produtos_carrinho.items():
+            if quantidade > 0:
+                validade_formatada: str = produto.validade.strftime("%d/%m/%Y")
+                preco_total = produto.preco * quantidade
+                preco_formatado: str = formatar_preco(preco_total)
+                print(
+                    f"{produto.id:<5} {produto.nome:<20} {produto.tipo:<15} {validade_formatada:<12} {quantidade:<13} {preco_formatado:<10}"
+                )
+
+    def fechar_pedido(self) -> None:
+        total_pedido = 0.0
+
+        print("--- Resumo do pedido ---")
+        print(
+            f"{'ID':<5} {'Nome':<20} {'Tipo':<15} {'Validade':<12} {'Quantidade':<13} {'Preço':<10}"
+        )
+        print("-" * 85)
+
+        for produto, quantidade in self.produtos_carrinho.items():
+            validade_formatada: str = produto.validade.strftime("%d/%m/%Y")
+            preco_total = produto.preco * quantidade
+            preco_formatado: str = formatar_preco(preco_total)
+            total_pedido += preco_total
+            print(
+                f"{produto.id:<5} {produto.nome:<20} {produto.tipo:<15} {validade_formatada:<12} {quantidade:<13} {preco_formatado:<10}"
+            )
+
+        total_formatado: str = formatar_preco(total_pedido)
+        print("-" * 85)
+        print(f"{'Total: ':>78}{total_formatado}")
